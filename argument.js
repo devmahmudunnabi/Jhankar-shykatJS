8ad71fb32eb6da362c411e051d7ce8d3faab0bd0
devmahmudunnabi <shykatworks@gmail.com>
2024-06-01 23:37:15 +0800

    Change Some ArgumentJS file

--- argument.js
+++ argument.js
@@ -4,6 +4,10 @@ function argument(num1 , num2) {
         const element = arguments[i];
         sum = sum + element
     }
+    var almamoon = "Ektah maindar";
+    var dadu = "Ektah Batpar";
+    var equal = almamoon + dadu;
+    console.log(equal);
     return sum;
 }
  var result = argument(20,10,30)
